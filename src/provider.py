0d4430021895c21a4986af81616a5fcbd98a366f
pmayorov <pmayorov@ujin.tech>
2025-03-04 10:03:32 +0500

    feat: add providers

---
+++ src/provider.py
@@ -0,0 +1,62 @@
+from typing import TYPE_CHECKING
+
+from . import models
+from .database import dto
+
+if TYPE_CHECKING:
+    from .database import Repository
+    from .headhunter import HeadHunter
+
+
+async def add_account(
+    db: "Repository",
+    hh: "HeadHunter",
+    account: models.Account,
+) -> None:
+    """Добавляет новый аккаунт в БД"""
+    await hh.get_account_auth_tokens(account)
+    db_account = dto.DBAccount(
+        login=account.login,
+        password=account.password,
+    )
+    await db.add_account(db_account)
+
+
+async def get_account_by_login(
+    db: "Repository",
+    login: str,
+) -> models.Account:
+    """Запрашивает и возвращает аккаунты из БД по логину"""
+    db_account = await db.get_account_by_login(login)
+    return models.Account.from_db(db_account)
+
+
+async def get_account_resumes(
+    hh: "HeadHunter",
+    account: models.Account,
+) -> list[models.Resume]:
+    """Запрашивает и возвращает все резюме аккаунта из БД"""
+    return await hh.get_resumes(account)
+
+
+async def add_resume(
+    db: "Repository",
+    account: models.Account,
+    resume: models.Resume,
+) -> None:
+    """Добавляет резюме в БД"""
+    db_resume = dto.DBResume(
+        resume_id=resume.resume_id,
+        title=resume.title,
+        account_login=account.login,
+    )
+    await db.add_resume(db_resume)
+
+
+async def get_all_accounts_resumes(
+    db: "Repository",
+) -> list[tuple[models.Account, models.Resume]]:
+    """Запрашивает и возвращает все резюме всех аккаунтов из БД"""
+    db_accounts_resumes = await db.get_all_accounts_resumes()
+    return [(models.Account.from_db(db_account), models.Resume.from_db(db_resume))
+            for db_account, db_resume in db_accounts_resumes]

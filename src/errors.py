91a8945a91de0bac83715afad78e61e98be19acc
pmayorov <pmayorov@ujin.tech>
2025-03-04 10:04:05 +0500

    feat: add errors

---
+++ src/errors.py
@@ -0,0 +1,18 @@
+class HHError(Exception):
+    """Базовая ошибка сервиса HeadHunter"""
+
+
+class HHResponseError(HHError):
+    """Ошибка ответа на запрос"""
+
+
+class DBError(Exception):
+    """Базовая ошибка базы данных"""
+
+
+class AlreadyExistsError(DBError):
+    """Запись уже существует в БД"""
+
+
+class NotExistsError(DBError):
+    """Запись не найдена в БД"""
